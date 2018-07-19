9a284409d86592be357f6ebbc560346696a52451
wander <junjiehuang@yeah.net>
2018-07-19 18:11:33 +0800

    init repo

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TODO LIST'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
