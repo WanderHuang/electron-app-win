3ceb5c596bdae5a4b8a337a61a65aaf1123df57c
wander <junjiehuang@yeah.net>
2018-07-23 20:31:32 +0800

    主页设定为文件列表

--- src/renderer/components/tabs/home.vue
+++ src/renderer/components/tabs/home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="elife-home-page markdown-body">
-    <div v-html="msg"></div>
+  <div class="elife-home-page">
+    <div class="home-page-list">
+      <div v-for="x in 100" class="list-item" @click="folderClicked(x)">
+        <md-icon class="md-size-4x list-item-icon">folder</md-icon>
+        <span class="list-item-text">{{msg}}.{{x}}</span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -8,22 +13,40 @@ export default {
   name: 'elife-home',
   data: () => {
     return {
-      msg: ''
+      msg: 'file'
     }
   },
-  created () {
-    let $this = this
-    this.$http
-      .get('http://localhost:3000/getArticleByName')
-      .then(function (response) {
-        $this.msg = $this.$marked(response.data)
-      })
+  methods: {
+    folderClicked (index) {
+      this.msg = index
+    }
   }
 }
 </script>
-<style>
+<style scoped>
 .elife-home-page {
   margin-top: 50px;
   height: 500px;
+}
+.home-page-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+.list-item {
+  width: 120px;
+  height: 140px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.list-item:hover {
+  cursor: pointer;
+  background-color: rgba(123, 123, 123, 0.5)
+}
+.list-item .list-item-icon {
+  color:rgba(249, 236, 137, 0.9);
+}
+.list-item .list-item-text {
+  align-content: center;
 }
 </style>
