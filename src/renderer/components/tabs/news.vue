a72c3d7acfe7fed213b214bcabc7bde19fdfc25b
wander <junjiehuang@yeah.net>
2018-07-23 20:31:51 +0800

    新闻页设定为获取后台md文件

--- src/renderer/components/tabs/news.vue
+++ src/renderer/components/tabs/news.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="elife-news-page">
-    新闻联播
+  <div class="elife-news-page markdown-body">
+    <div v-html="msg"></div>
   </div>
 </template>
 <script>
   export default {
-    name: 'elife-news'
+    name: 'elife-news',
+    data: () => {
+      return {
+        msg: ''
+      }
+    },
+    created () {
+      let $this = this
+      this.$http
+        .get('http://localhost:3000/getArticleByName')
+        .then(function (response) {
+          $this.msg = $this.$marked(response.data)
+        })
+    }
   }
 </script>
 <style>
