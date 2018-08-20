187c4b9838adcb9268398f6e0439ba0619a52b52
wander <junjiehuang@yeah.net>
2018-08-20 21:56:12 +0800

    增加跨域头

--- backend/main.js
+++ backend/main.js
@@ -11,7 +11,7 @@ app.use(bodyParser())
 
 app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*')
-  ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+  ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Content-Length, Authorization, Accept')
   ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   await next()
  })
