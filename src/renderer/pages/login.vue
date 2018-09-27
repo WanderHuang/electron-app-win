ae7ebfa8a5e67ced4ef9d5ec6ebdc365740aaa61
wander <junjiehuang@yeah.net>
2018-09-27 16:03:13 +0800

    refactor(redecorate login page): refactor login page with scroll text. add some color

--- src/renderer/pages/login.vue
+++ src/renderer/pages/login.vue
@@ -1,22 +1,30 @@
 <template>
-  <el-container class="ehome-login">
-    <el-header class="header">WELCOME TO HOME PET!</el-header>
-    <el-container class="form-wrapper">
-      <el-main>
-        <el-form ref="form" :model="form" :rules="rules" label-width="80px" class="form">
-          <el-form-item label="账户号码" prop="username">
-            <el-input v-model="form.username"></el-input>
-          </el-form-item>
-          <el-form-item label="用户密码" prop="password">
-            <el-input type="password" auto-complete="off" v-model="form.password"></el-input>
-          </el-form-item>
-        </el-form>
-        <div class="button-wrapper">
-          <el-button type="success" round @click.stop="regist">注册</el-button>
-          <el-button type="primary" round @click.stop="login">登录</el-button>
+  <el-container class="ehome-login" direction="horizontal">
+    <el-main class="login-background">
+      <div class="scroll-words-wrapper">
+        <span>Hope you be</span>
+        <div class="scroll-item-wrapper">
+          <span>HAPPY</span>
+          <span>SWEET</span>
+          <span>OPTIMISM</span>
+          <span>POSITIVE</span>
         </div>
-      </el-main>
-    </el-container>
+      </div>
+    </el-main>
+    <el-aside width="400px" class="form-wrapper">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px" width="300px" class="form-list">
+        <el-form-item label="账户号码" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码" prop="password">
+          <el-input type="password" auto-complete="off" v-model="form.password"></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="button-wrapper">
+        <el-button type="success" @click.stop="regist">注册</el-button>
+        <el-button type="primary" @click.stop="login">登录</el-button>
+      </div>
+    </el-aside>
   </el-container>
 </template>
 <script>
@@ -65,27 +73,103 @@ export default {
 }
 </script>
 <style lang="less">
+@full-size: 100%;
+@word-height: 48px;
+@bg-degree: 120deg;
+@form-width: 300px;
 .ehome-login {
-  height: 600px;
-  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
-  .header {
-    text-align: center;
-    height: 40px;
-    line-height: 40px;
-    font-size: 20px;
-    font-family: 'Courier New', Courier, monospace
-  }
-  .form-wrapper{
-    padding-right: 100px;
-    padding-left: 200px;
-    border: 2px dashed lightseagreen;
-    .form {
-      padding: 100px 0px 20px 100px;
+  height: @full-size;
+  .login-background {
+    position: relative;
+    background-image: linear-gradient(@bg-degree, #e0c3fc 0%, #8ec5fc @full-size);
+    .scroll-words-wrapper {
+      width: @full-size;
+      height: @word-height;
+      overflow: hidden;
+      display: flex;
+      justify-content: center;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      font-size: @word-height - 5;
+      & span {
+        color: #66b1ff;  
+        text-align: center;
+        flex-basis: 50%;
+      }
+      .scroll-item-wrapper {
+        display: flex;
+        flex-direction: column;
+        height: @word-height;
+        width: 200px;
+        animation: scroll-words 12s infinite;
+        & span {
+          height: @word-height;
+        }
+        & span:nth-child(1) {
+          color: #e0c3fc;
+        }
+        & span:nth-child(2) {
+          color: #f9a383;
+        }
+        & span:nth-child(3) {
+          color: lightgreen;
+        }
+        & span:nth-child(4) {
+          color: #67c23a;
+        }
+      }
     }
   }
-  .button-wrapper{
+  .form-wrapper {
     display: flex;
-    justify-content: flex-end;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-image: linear-gradient(@bg-degree,#8ec5fc 0,  #e0c3fc @full-size);
+    .form-list {
+      width: @form-width;
+      .el-form-item__label {
+        background-color: transparent;
+      }
+      .el-input__inner {
+        background-color: transparent;
+        border-color: #66b1ff;
+        color: #000;
+      }
+    }
+    .button-wrapper{
+      display: flex;
+      width: @form-width;
+      height: 40px;
+      button {
+        width: @full-size;
+      }
+    }
+  }
+}
+
+@keyframes scroll-words {
+  0% {
+    transform: translate3d(0, @word-height, 0)
+  }
+  20% {
+    transform: translate3d(0, 0px, 0)
+  }
+  40% {
+    transform: translate3d(0, -(@word-height), 0)
+  }
+  60% {
+    transform: translate3d(0, -(@word-height * 2), 0)
+  }
+  80% {
+    transform: translate3d(0, -(@word-height * 3), 0)
+  }
+  99.9% {
+    transform: translate3d(0, -(@word-height * 4), 0)
+  }
+  100% {
+    transform: translate3d(0, -360px, 0)
   }
 }
 </style>
